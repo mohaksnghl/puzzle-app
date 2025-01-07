fe9c553a3617a3be6b671b0631c88c026f81df57
Mohak Singhal <sinmohak@amazon.com>
2025-01-07 00:35:33 -0500

    fix sound

--- src/constants.js
+++ src/constants.js
@@ -3,10 +3,14 @@ export const INITIAL_TIME = 60; // Initial countdown time in seconds
 export const TIME_BONUS = 5; // Bonus time added for correct answers
 
 // Sound file paths
-export const CLOCK_TICK_SOUND = "/sound/clock-tick.mp3";
-export const TIMES_UP_SOUND = "/sound/times-up.mp3";
-export const CORRECT_ANSWER_SOUND = "/sound/correct-answer.mp3";
-export const CORRECT_WORD_SOUND = "/sound/correct-word.mp3";
+export const CLOCK_TICK_SOUND =
+  "https://fiesta-media.s3.amazonaws.com/clock-tick.mp3";
+export const TIMES_UP_SOUND =
+  "https://fiesta-media.s3.amazonaws.com/times-up.mp3";
+export const CORRECT_ANSWER_SOUND =
+  "https://fiesta-media.s3.amazonaws.com/correct-answer.mp3";
+export const CORRECT_WORD_SOUND =
+  "https://fiesta-media.s3.amazonaws.com/correct-word.mp3";
 export const TICK_SOUND_VOL = 0.1;
 export const TIMES_UP_SOUND_VOL = 0.6;
 
