da4f3030862bb00057ef43cd8b09684e569c4915
Mohak Singhal <sinmohak@amazon.com>
2025-01-08 03:02:46 -0500

    Revert "Fix bgm not playing issue"
    This reverts commit 74e17df05d453bcd02c8b38aa608319345c62c20.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { MOCK_WORD_PAIR, GOOGLE_LOGO, GAME_BGM } from "./constants";
 import { v4 as uuidv4 } from "uuid";
 
-import { MainContainer, LogoContainer, LogoImage } from "./StyledComponents";
+import {
+  MainContainer,
+  HowToPlayContainer,
+  InstructionIcon,
+  InstructionItem,
+  LogoContainer,
+  LogoImage,
+} from "./StyledComponents";
 
 import logo from "./assets/logo-image.png"; // Path to your PNG
 
@@ -18,7 +25,6 @@ let bgmAudio;
 const MainScreen = () => {
   const [user, setUser] = useState(null);
   const [userId, setUserId] = useState("");
-  const [showOverlay, setShowOverlay] = useState(true); // Show overlay initially
   const [name, setName] = useState("");
   const [gameStarted, setGameStarted] = useState(false);
   const [hint1, setHint1] = useState("");
@@ -65,18 +71,6 @@ const MainScreen = () => {
     setGameStarted(true);
   };
 
-  const handleOverlayButtonClick = () => {
-    if (!bgmAudio) {
-      bgmAudio = new Audio(GAME_BGM);
-      bgmAudio.loop = true;
-      bgmAudio.volume = 0.5;
-      bgmAudio
-        .play()
-        .catch((error) => console.error("Error playing BGM:", error));
-    }
-    setShowOverlay(false); // Hide overlay after user interaction
-  };
-
   useEffect(() => {
     if (!bgmAudio) {
       // Initialize the BGM only once
@@ -104,196 +98,146 @@ const MainScreen = () => {
     };
   }, [gameStarted]); // Triggered only when gameStarted changes
 
-  return (
-    <>
-      {showOverlay && (
+  return gameStarted ? (
+    <GameScreen
+      userId={userId}
+      name={name}
+      hint1={hint1}
+      hint2={hint2}
+      word1={word1}
+      word2={word2}
+      refreshWords={refreshWords}
+    />
+  ) : (
+    <MainContainer>
+      <LogoContainer>
+        <LogoImage src={logo} alt="Two Words Game Logo" />
+      </LogoContainer>
+
+      <Box>
         <Box
+          component="button"
+          onClick={user ? handleLogout : handleGoogleLogin}
           style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.9)",
-            color: "#fff",
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
             justifyContent: "center",
-            zIndex: 1000, // Ensure it is above other elements
-            padding: "20px", // Add padding for responsiveness
+            padding: "12px 20px",
+            fontSize: "1rem",
+            fontWeight: "bold",
+            color: Colors.primary,
+            borderRadius: "30px",
+            cursor: "pointer",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
+            textTransform: "none",
+            margin: "20px auto",
+            width: "fit-content",
           }}
         >
-          <Typography
-            style={{
-              fontSize: "2rem",
-              fontWeight: "bold",
-              textAlign: "center",
-              marginBottom: "30px",
-            }}
-          >
-            Welcome to the Two Words, One Letter Apart Game!
-          </Typography>
-          <Button
-            onClick={handleOverlayButtonClick}
-            style={{
-              padding: "15px 30px",
-              fontSize: "1.2rem",
-              fontWeight: "bold",
-              backgroundColor: Colors.backgroundMain,
-              color: Colors.primary,
-              borderRadius: "30px",
-              cursor: "pointer",
-              border: `2px solid ${Colors.primary}`, // Optional border for emphasis
-            }}
-          >
-            Start
-          </Button>
-        </Box>
-      )}
-      {gameStarted ? (
-        <GameScreen
-          userId={userId}
-          name={name}
-          hint1={hint1}
-          hint2={hint2}
-          word1={word1}
-          word2={word2}
-          refreshWords={refreshWords}
-        />
-      ) : (
-        <MainContainer>
-          <LogoContainer>
-            <LogoImage src={logo} alt="Two Words Game Logo" />
-          </LogoContainer>
-
-          <Box>
-            <Box
-              component="button"
-              onClick={user ? handleLogout : handleGoogleLogin}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                padding: "12px 20px",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                color: Colors.primary,
-                borderRadius: "30px",
-                cursor: "pointer",
-                boxShadow: "0 4px 10px rgba(0, 0, 0, 0.1)",
-                textTransform: "none",
-                margin: "20px auto",
-                width: "fit-content",
-              }}
-            >
-              {user ? (
-                <>
-                  <Box
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <img
-                      src={GOOGLE_LOGO}
-                      alt="Profile"
-                      style={{
-                        width: "24px",
-                        height: "24px",
-                        marginRight: "10px",
-                        borderRadius: "50%",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <span
-                      style={{
-                        whiteSpace: "nowrap",
-                      }}
-                    >
-                      Logged in as {user.displayName}
-                    </span>
-                  </Box>
-                  <Box
-                    style={{
-                      marginTop: "4px", // Minimal gap between main text and subtext
-                      fontSize: "0.8rem", // Smaller font for subtext
-                      color: Colors.darkGray, // Subtle text color for subtext
-                    }}
-                  >
-                    Click to Logout
-                  </Box>
-                </>
-              ) : (
-                <>
-                  <Box
-                    style={{
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                    }}
-                  >
-                    <img
-                      src={GOOGLE_LOGO}
-                      alt="Google Logo"
-                      style={{
-                        width: "20px",
-                        height: "20px",
-                        marginRight: "10px",
-                      }}
-                    />
-                    <span>Login with Google</span>
-                  </Box>
-                </>
-              )}
-            </Box>
-
-            {!user && (
+          {user ? (
+            <>
               <Box
-                component="input"
-                placeholder="Enter your name to play as Guest"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
                 style={{
-                  marginBottom: "1.5rem",
-                  padding: "10px",
-                  width: "80%",
-                  maxWidth: "400px",
-                  fontSize: "1.2rem",
-                  outline: "none", // Remove default browser focus outline
-                  boxShadow: "none", // Remove any box-shadow on focus
-                  color: Colors.primary,
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
                 }}
-              />
-            )}
+              >
+                <img
+                  src={GOOGLE_LOGO}
+                  alt="Profile"
+                  style={{
+                    width: "24px",
+                    height: "24px",
+                    marginRight: "10px",
+                    borderRadius: "50%",
+                    objectFit: "cover",
+                  }}
+                />
+                <span
+                  style={{
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  Logged in as {user.displayName}
+                </span>
+              </Box>
+              <Box
+                style={{
+                  marginTop: "4px", // Minimal gap between main text and subtext
+                  fontSize: "0.8rem", // Smaller font for subtext
+                  color: Colors.darkGray, // Subtle text color for subtext
+                }}
+              >
+                Click to Logout
+              </Box>
+            </>
+          ) : (
+            <>
+              <Box
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <img
+                  src={GOOGLE_LOGO}
+                  alt="Google Logo"
+                  style={{
+                    width: "20px",
+                    height: "20px",
+                    marginRight: "10px",
+                  }}
+                />
+                <span>Login with Google</span>
+              </Box>
+            </>
+          )}
+        </Box>
 
-            <Box
-              component="button"
-              onClick={handleStartGame}
-              disabled={!name.trim()}
-              style={{
-                padding: "15px 30px",
-                fontSize: "1.2rem",
+        {!user && (
+          <Box
+            component="input"
+            placeholder="Enter your name to play as Guest"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{
+              marginBottom: "1.5rem",
+              padding: "10px",
+              width: "80%",
+              maxWidth: "400px",
+              fontSize: "1.2rem",
+              outline: "none", // Remove default browser focus outline
+              boxShadow: "none", // Remove any box-shadow on focus
+              color: Colors.primary,
+            }}
+          />
+        )}
 
-                textTransform: "none",
-                backgroundColor: name.trim()
-                  ? Colors.primary
-                  : Colors.backgroundMain,
-                color: name.trim() ? Colors.backgroundMain : Colors.primary, // Change text color for outlined button
-                borderRadius: "30px",
-                cursor: name.trim() ? "pointer" : "not-allowed",
-                border: `4px solid ${Colors.primary}`,
-              }}
-            >
-              {user ? "Start Game" : "Play as Guest"}
-            </Box>
-          </Box>
-        </MainContainer>
-      )}
-      ;
-    </>
+        <Box
+          component="button"
+          onClick={handleStartGame}
+          disabled={!name.trim()}
+          style={{
+            padding: "15px 30px",
+            fontSize: "1.2rem",
+
+            textTransform: "none",
+            backgroundColor: name.trim()
+              ? Colors.primary
+              : Colors.backgroundMain,
+            color: name.trim() ? Colors.backgroundMain : Colors.primary, // Change text color for outlined button
+            borderRadius: "30px",
+            cursor: name.trim() ? "pointer" : "not-allowed",
+            border: `4px solid ${Colors.primary}`,
+          }}
+        >
+          {user ? "Start Game" : "Play as Guest"}
+        </Box>
+      </Box>
+    </MainContainer>
   );
 };
 
